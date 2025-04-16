b4a9cb124f203e98f343bc9c4fd7be25691a50b7
meruyert4 <meruyertbauyrzhanqyzy@gmail.com>
2025-04-16 17:59:43 +0500

    profile page

--- lib/screens/auth_wrapper.dart
+++ lib/screens/auth_wrapper.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:recipe_app/screens/home_screen.dart';
-import 'package:recipe_app/screens/auth/login_screen.dart';
+import 'package:recipe_app/custom_navbar.dart;
 
 class AuthWrapper extends StatelessWidget {
   const AuthWrapper({Key? key}) : super(key: key);
@@ -11,15 +10,16 @@ class AuthWrapper extends StatelessWidget {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        } else if (snapshot.hasData) {
-          return const HomeScreen(); // Authenticated
-        } else {
-          return const LoginScreen(); // Not authenticated
+        if (snapshot.connectionState == ConnectionState.active) {
+          // If the user is logged in, navigate to CustomNavBar
+          if (snapshot.hasData) {
+            return const CustomNavBar();
+          }
+          // If the user is not logged in, show the login screen (or other UI)
+          return const LoginScreen();
         }
+        // Show loading spinner while checking auth state
+        return const CircularProgressIndicator();
       },
     );
   }
